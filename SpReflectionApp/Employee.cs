0705f83cc716f818a6386a50b081df02689eeb52
Maxim Efimov <efimov_m@t.top-academy.ru>
2023-11-12 17:11:01 +0300

    Добавьте файлы проекта.

---
+++ SpReflectionApp/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpReflectionApp
+{
+
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public interface IWorkable
+    {
+        void Work();
+    }
+
+
+    public class Employee : IWorkable, IMovable
+    {
+        int id = -1;
+
+        public string? Name { get; set; }
+        public int Age { get; }
+
+        public Employee()
+        {
+            Name = "Anonim";
+            Age = 0;
+        }
+
+        public Employee(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Work()
+        {
+            Console.WriteLine($"{Name} work");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine($"{Name} move");
+        }
+
+        public int AgeMult(int mult = 1)
+        {
+            return Age * mult;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee id:{id} name: {Name}, age: {Age}";
+        }
+    }
+}
---
+++ SpReflectionApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Reflection;
+using SpReflectionApp;
+
+
+//Type typeEmployee = typeof(Employee);
+//Type typeEmployee = Type.GetType("SpReflectionApp.Employee", false, true);
+Employee bob = new Employee("Bob", 45);
+Type typeEmployee = bob.GetType();
+
+Console.WriteLine(typeEmployee);
+
+foreach(var m in typeEmployee.GetMembers())
+    Console.WriteLine($"{m.DeclaringType} {m.MemberType} {m.Name}");
+Console.WriteLine();
+
+foreach (var m in typeEmployee.GetMembers(BindingFlags.DeclaredOnly
+    | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
+    Console.WriteLine($"{m.DeclaringType} {m.MemberType} {m.Name}");
+Console.WriteLine();
+
+
+foreach (var p in typeEmployee.GetProperties())
+    Console.WriteLine(p.Name);
+Console.WriteLine();
+
+foreach (var f in typeEmployee.GetFields(BindingFlags.NonPublic | BindingFlags.Instance))
+    Console.WriteLine(f.Name);
+Console.WriteLine();
+
+foreach (var i in typeEmployee.GetInterfaces())
+    Console.WriteLine(i.Name);
+Console.WriteLine();
+
+var work = typeEmployee.GetMethod("Work");
+work.Invoke(bob, null);
+Console.WriteLine(work.IsPublic);
+Console.WriteLine(work.ReturnType);
+
+Console.WriteLine();
+var ageMult = typeEmployee.GetMethod("AgeMult");
+var pars = ageMult.GetParameters();
+foreach(var p in pars)
+{
+    Console.WriteLine(p.Name);
+    Console.WriteLine(p.ParameterType);
+    Console.WriteLine(p.HasDefaultValue);
+    Console.WriteLine(p.DefaultValue);
+
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+var ctors = typeEmployee.GetConstructors();
+
+var ctorParams = typeEmployee.GetConstructor(new Type[] { typeof(string), typeof(int) });
+var e = ctorParams.Invoke(new object[] { "Joe", 33 });
+
+Console.WriteLine(e);
+
+
+var ctorDefault = typeEmployee.GetConstructor(Type.EmptyTypes);
+var eAnonim = ctorDefault.Invoke(new object[] {});
+
+Console.WriteLine(eAnonim);
+
+var nameProp = typeEmployee.GetProperty("Name");
+nameProp.SetValue(eAnonim, "Tom");
+
+var idField = typeEmployee.GetField("id", BindingFlags.NonPublic | BindingFlags.Instance);
+idField?.SetValue(eAnonim, 5);
+
+Console.WriteLine(eAnonim);
